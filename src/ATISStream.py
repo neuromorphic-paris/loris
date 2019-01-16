ba0b288b79f235058b6f94cf81640eb6fe37bdf3
Gregor Lenz <mail@lenzgregor.com>
2019-01-16 17:02:00 +0100

    fix ATIS stream read and write

--- src/ATISStream.py
+++ src/ATISStream.py
@@ -1,11 +1,10 @@
-
 import numpy as np
 import os
 from .EventStream import EventStream
 from .config import VERSION, TYPE
 
-ATIStype = [('x',np.uint16), ('y', np.uint16),
-            ('p', np.bool_), ('isTc', np.bool_), ('ts', np.uint64)]
+ATIStype = [('x', np.uint16), ('y', np.uint16),
+            ('p', np.bool_), ('is_tc', np.bool_), ('ts', np.uint64)]
 
 
 class ATISStream(EventStream):
@@ -16,37 +15,36 @@ class ATISStream(EventStream):
         self.width = np.uint16(_width)
         self.height = np.uint16(_height)
 
-    def read(event_data):
-        width = event_data[17] << 8 + event_data[16]
-        height = event_data[19] << 8 + event_data[18]
-        f_cursor = 20
+    def read(event_data, version):
+        width = (event_data[17] << 8) + event_data[16]
+        height = (event_data[19] << 8) + event_data[18]
+        file_cursor = 20
         end = len(event_data)
         events = []
-        currentTime = 0
-        while(f_cursor < end):
-            byte = event_data[f_cursor]
+        current_time = 0
+        while(file_cursor < end):
+            byte = event_data[file_cursor]
             if byte & 0xfc == 0xfc:
                 if byte == 0xfc:  # Reset event
                     pass
                 else:  # Overflow event
-                    currentTime += (byte & 0x03) * 64
-
+                    current_time += (byte & 0x03) * 63
             else:
-                f_cursor += 1
-                byte1 = ESdate[f_cursor]
-                f_cursor += 1
-                byte2 = ESfile[f_cursor]
-                f_cursor += 1
-                byte3 = ESfile[f_cursor]
-                f_cursor += 1
-                byte4 = ESfile[f_cursor]
-                currentTime += (byte >> 2)
+                file_cursor += 1
+                byte1 = event_data[file_cursor]
+                file_cursor += 1
+                byte2 = event_data[file_cursor]
+                file_cursor += 1
+                byte3 = event_data[file_cursor]
+                file_cursor += 1
+                byte4 = event_data[file_cursor]
+                current_time += (byte >> 2)
                 x = ((byte2 << 8) | byte1)
                 y = ((byte4 << 8) | byte3)
-                isTc = (byte & 0x01)
+                is_tc = (byte & 0x01)
                 p = ((byte & 0x02) >> 1)
-                events.append((x, y, p, isTc, currentTime))
-            f_cursor += 1
+                events.append((x, y, p, is_tc, current_time))
+            file_cursor += 1
         return ATISStream(width, height, events, version)
 
     def write(self, filename):
@@ -58,7 +56,6 @@ class ATISStream(EventStream):
             except OSError as exc:  # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
-
         to_write = bytearray('Event Stream', 'ascii')
         to_write.append(int(self.version.split('.')[0]))
         to_write.append(int(self.version.split('.')[1]))
@@ -68,24 +65,24 @@ class ATISStream(EventStream):
         to_write.append(np.uint8(self.width >> 8))
         to_write.append(np.uint8(self.height))
         to_write.append(np.uint8(self.height >> 8))
-        previousTs = 0
+        previous_ts = 0
         for datum in self.data:
-            relativeTs = datum.ts - previousTs
-            if relativeTs > 63:
-                numberOfOverflows = int(relativeTs / 64)
-                for i in range(int(numberOfOverflows/3)):
+            relative_ts = datum.ts - previous_ts
+            if relative_ts >= 63:
+                number_of_overflows = int(relative_ts / 63)
+                relative_ts -= number_of_overflows * 63
+                for i in range(int(number_of_overflows/3)):
                     to_write.append(0xff)
-                numberOfOverflowsLeft = numberOfOverflows % 3
-                if numberOfOverflowsLeft > 0:
-                    to_write.append(0xfc | np.uint8(numberOfOverflowsLeft))
-                relativeTs -= numberOfOverflows * 64
-            to_write.append(np.uint8((relativeTs << 2) & 0xfc)
-                            | (datum.p << 1) | (datum.isTc))
+                number_of_overflows = number_of_overflows % 3
+                if number_of_overflows > 0:
+                    to_write.append(0xfc | np.uint8(number_of_overflows))
+            to_write.append(np.uint8((np.uint8(relative_ts) << 2) & 0xfc)
+                            | (datum.p << 1) | datum.is_tc)
             to_write.append(np.uint8(datum.x))
             to_write.append(np.uint8(datum.x >> 8))
             to_write.append(np.uint8(datum.y))
             to_write.append(np.uint8(datum.y >> 8))
-            previousTs = datum.ts
-        ES_file = open(filename, 'wb')
-        ES_file.write(to_write)
-        ES_file.close()
+            previous_ts = datum.ts
+        file = open(filename, 'wb')
+        file.write(to_write)
+        file.close()
