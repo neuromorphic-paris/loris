5880bec3ebf3c0e9012c901345a11c5275aef7fb
Gregor Lenz <mail@lenzgregor.com>
2019-02-01 19:45:26 +0100

    add check_incoherent_events function

--- loris/ReadFile.py
+++ loris/ReadFile.py
@@ -11,12 +11,26 @@ def read_file(file_name):
         print("Not yet implemented")
         return None
     elif file_name.endswith('.dat'):
-        return dat.parse_file(file_name)
+        events = dat.parse_file(file_name)
     elif file_name.endswith('.es'):
-        return event_stream.parse_file(file_name)
+        events = event_stream.parse_file(file_name)
     elif file_name.endswith('.csv'):
-        return csv.parse_file(file_name)
+        events = csv.parse_file(file_name)
     else:
         print("I don't know what kind of format you want to read. "
               + "Please specify a valid file name ending such as .aedat etc")
         return None
+    check_incoherent_events(events)
+    return events
+
+def check_incoherent_events(events):
+    indices = []
+    for index, event in enumerate(events):
+        if index > 0 and events['ts'][index] < events['ts'][index-1]:
+            indices.append(index)
+            #print("Timestamp index - 1: "+ str(index-1) + ", index: " + str(index))
+    if len(indices) == 0:
+        print("All timestamps in correct order.")
+    else:
+        print("Not all timestamps are in ascending order.")
+    return
