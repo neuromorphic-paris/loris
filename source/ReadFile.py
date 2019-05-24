666314c555a1bac0f0d48723e08dff057e72a242
Gregor Lenz <mail@lenzgregor.com>
2019-05-24 10:55:02 +0200

    add fallback for .dat

--- source/ReadFile.py
+++ source/ReadFile.py
@@ -17,15 +17,19 @@ def read_file(file_name):
     if file_name.endswith('.aedat'):
         print("Not yet implemented")
         return None
+    # dat
+    elif file_name.endswith('.dat') and is_using_fallback:
+        parsed_file = dat.parse_file(file_name)
     elif file_name.endswith('.dat') and '_td' in file_name:
-        print('Reading dat td file')
         parsed_file = loris_extension.read_dat_td(file_name)
     elif file_name.endswith('.dat') and '_aps' in file_name:
         parsed_file = loris_extension.read_dat_aps(file_name)
+    # EventStream
     elif file_name.endswith('.es') and is_using_fallback:
         parsed_file = event_stream.parse_file(file_name)
     elif file_name.endswith('.es'):
         parsed_file = loris_extension.read_event_stream(file_name)
+    # csv
     elif file_name.endswith('.csv'):
         parsed_file = csv.parse_file(file_name)
     else:
@@ -38,6 +42,6 @@ def read_file(file_name):
 def check_incoherent_events(parsed_file):
     for i in range(0, len(parsed_file['events'])):
         if i > 0 and parsed_file['events']['t'][i-1] > parsed_file['events']['t'][i]:
-            print('Not all timestamps are in ascending order.')
+            print('Not all timestamps are in ascending order, just so you know.')
             break
     return
