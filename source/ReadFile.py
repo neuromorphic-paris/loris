e44a277e10fde322c67d5f16e89e43c7b5f2f23a
Gregor Lenz <mail@lenzgregor.com>
2019-05-24 11:23:02 +0200

    remove fallback options for .es and .dat

--- source/ReadFile.py
+++ source/ReadFile.py
@@ -1,13 +1,7 @@
 from . import EventStream as event_stream
 from . import DAT as dat
 from . import CSV as csv
-
-is_using_fallback = False
-try:
-    import loris_extension
-except ImportError:
-    is_using_fallback = True
-    print("Could not import c++ extension to speed up things. Falling back to pure Python implementation...")
+import loris_extension
 
 
 def read_file(file_name):
@@ -17,19 +11,12 @@ def read_file(file_name):
     if file_name.endswith('.aedat'):
         print("Not yet implemented")
         return None
-    # dat
-    elif file_name.endswith('.dat') and is_using_fallback:
-        parsed_file = dat.parse_file(file_name)
     elif file_name.endswith('.dat') and '_td' in file_name:
         parsed_file = loris_extension.read_dat_td(file_name)
     elif file_name.endswith('.dat') and '_aps' in file_name:
         parsed_file = loris_extension.read_dat_aps(file_name)
-    # EventStream
-    elif file_name.endswith('.es') and is_using_fallback:
-        parsed_file = event_stream.parse_file(file_name)
     elif file_name.endswith('.es'):
         parsed_file = loris_extension.read_event_stream(file_name)
-    # csv
     elif file_name.endswith('.csv'):
         parsed_file = csv.parse_file(file_name)
     else:
