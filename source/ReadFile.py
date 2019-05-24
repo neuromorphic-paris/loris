5c222564cf848eb02db47b34229bc315f87d331b
Gregor Lenz <mail@lenzgregor.com>
2019-05-24 17:54:41 +0200

    add optional second parameter to combine dat_td and dat_aps files

--- source/ReadFile.py
+++ source/ReadFile.py
@@ -2,17 +2,19 @@ from . import CSV as csv
 import loris_extension
 
 
-def read_file(file_name):
+def read_file(file_name, file_name_dat_aps=None):
     """parse a file from a neuromorphic camera and return events
     supported file formats are .aedat, .dat, .es and .csv
     """
     if file_name.endswith('.aedat'):
-        print("Not yet implemented ok")
+        print("Not yet implemented")
         return None
-    elif file_name.endswith('.dat') and '_td' in file_name:
+    elif file_name.endswith('.dat') and '_td' in file_name and file_name_dat_aps == None:
         parsed_file = loris_extension.read_dat_td(file_name)
-    elif file_name.endswith('.dat') and '_aps' in file_name:
+    elif file_name.endswith('.dat') and '_aps' in file_name and file_name_dat_aps == None:
         parsed_file = loris_extension.read_dat_aps(file_name)
+    elif file_name.endswith('.dat') and file_name_dat_aps.endswith('.dat'):
+        parsed_file = loris_extension.read_dat_td_aps(file_name, file_name_dat_aps)
     elif file_name.endswith('.es'):
         parsed_file = loris_extension.read_event_stream(file_name)
     elif file_name.endswith('.csv'):
@@ -22,5 +24,8 @@ def read_file(file_name):
               + "Please specify a valid file name ending such as .aedat etc")
         return None
 
-    print("Read " + str(len(parsed_file['events'])) + " events from " + parsed_file['type'] + " file.")
+    if file_name_dat_aps == None:
+        print("Read " + str(len(parsed_file['events'])) + " events from file with " + parsed_file['type'] + " events.")
+    else:
+        print("Read " + str(len(parsed_file['events'])) + " events from combined files with dvs and atis events.")
     return parsed_file
