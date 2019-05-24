02c86ddee23d8a07f89e1ac8daa7cc3080791cf9
Gregor Lenz <mail@lenzgregor.com>
2019-05-24 17:53:56 +0200

    remove check_incoherent_events as python implementation is slow

--- source/ReadFile.py
+++ source/ReadFile.py
@@ -21,14 +21,6 @@ def read_file(file_name):
         print("I don't know what kind of format you want to read. "
               + "Please specify a valid file name ending such as .aedat etc")
         return None
-    check_incoherent_events(parsed_file)
 
     print("Read " + str(len(parsed_file['events'])) + " events from " + parsed_file['type'] + " file.")
     return parsed_file
-
-def check_incoherent_events(parsed_file):
-    for i in range(0, len(parsed_file['events'])):
-        if i > 0 and parsed_file['events']['t'][i-1] > parsed_file['events']['t'][i]:
-            print('Not all timestamps are in ascending order, just so you know.')
-            break
-    return
