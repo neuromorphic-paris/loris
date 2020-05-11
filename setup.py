d9dcb4ded150f16b002ac3268bb6ad7d2c7d4574
Gregor Lenz <mail@lenzgregor.com>
2020-05-11 10:10:31 +0100

    bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ with open('README.md', 'r') as file:
 # setup the package
 setuptools.setup(
     name='loris',
-    version='0.4.2',
+    version='0.5.0',
     url='https://github.com/neuromorphic-paris/loris',
     author='Gregor Lenz, Alexandre Marcireau',
     author_email='gregor.lenz@inserm.fr, alexandre.marcireau@gmail.com',
